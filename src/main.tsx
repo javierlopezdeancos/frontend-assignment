603f5f9dd352645f3a6bb036fc532cc7c533c426
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-05-23 20:52:58 +0200

    chore: add router to main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import rootRoute from '@/routes/root-route.tsx'
+import moviesRoute from '@/routes/shows/shows-route'
+import ErrorBoundaryComponent from '@/components/error-boundary/error-boundary-component.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const routeTree = rootRoute.addChildren([moviesRoute])
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('root')!
+
+if (!rootElement.innerHTML) {
+  const root = createRoot(rootElement)
+
+  root.render(
+    <StrictMode>
+      <ErrorBoundaryComponent fallback={<div>Error</div>}>
+        <RouterProvider router={router} />
+      </ErrorBoundaryComponent>
+    </StrictMode>,
+  )
+}
+
