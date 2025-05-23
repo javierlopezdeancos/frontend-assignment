73efeb4dd54e3e1ffab01bc973a7bd69cbd008b4
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-05-23 20:50:25 +0200

    chore: set proyect configurations and dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 /// <reference types="vitest" />
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   test: {
     globals: true,
     environment: 'jsdom',
