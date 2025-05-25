85d2bef149015c5628c9a2825a20f3486952f6b2
Javier Lopez de Ancos <javierlopezdeancos@gmail.com>
2025-05-25 22:52:07 +0200

    fix: fix not passing tests

--- tests/e2e/shows.spec.ts
+++ tests/e2e/shows.spec.ts
@@ -2,11 +2,11 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto("http://localhost:5173");
-  await expect(page.getByRole('heading', { name: 'shows' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '🍿 Shows' })).toBeVisible();
 });
 
 test('click into a show card link should go to show details page', async ({ page }) => {
   await page.goto("http://localhost:5173");
   await page.locator('#shows-list-1').first().click()
-  await expect(page.getByRole('heading', { name: 'show' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: '🎞️ Under the Dome' })).toBeVisible();
 });
